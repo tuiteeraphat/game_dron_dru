81cb8ff0836263ea9622d7959b3255f5222f14a6
Teeraphat Saengkum <kteeraphatt@gmail.com>
2023-10-29 17:13:32 +0700

    update graphic, model, sensen

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,24 @@
 import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
+import {
+  EffectComposer,
+  HueSaturation,
+  SMAA,
+  Glitch,
+} from "@react-three/postprocessing";
+import { BlendFunction } from "postprocessing";
 import CardDetailUser from "@/components/CardDetailUser";
 import {
   PerspectiveCamera,
   Environment,
   OrbitControls,
 } from "@react-three/drei";
-import SphereEnv from "@/components/gameComponents/SphereEnv";
-import Landscape from "@/components/gameComponents/Landscape";
 import GuiSetting from "@/components/gameComponents/GuiSetting";
+import Landscape from "@/components/gameComponents/Landscape";
+import SphereEnv from "@/components/gameComponents/SphereEnv";
+import { Airplane } from "@/components/gameComponents/Airplane";
+import { MotionBlur } from "@/components/gameComponents/MotionBlur";
+
 export default function Home() {
   return (
     <main className="w-screen h-screen relative flex flex-col items-center justify-between">
@@ -27,6 +37,9 @@ export default function Home() {
           {/* map load */}
           <Landscape />
 
+          {/* drone load */}
+          <Airplane />
+
           <directionalLight
             castShadow
             color={"#f3d29a"}
@@ -42,6 +55,20 @@ export default function Home() {
             shadow-camera-left={-6.2}
             shadow-camera-right={6.4}
           />
+
+          <EffectComposer multisampling={0}>
+            <SMAA />
+          </EffectComposer>
+
+          <EffectComposer>
+            {/* <Glitch /> */}
+            <MotionBlur />
+            <HueSaturation
+              blendFunction={BlendFunction.NORMAL}
+              hue={-0.15}
+              saturation={0.1}
+            />
+          </EffectComposer>
         </Suspense>
       </Canvas>
     </main>
